9ba12da69f090823725269c33b31886eb64af087
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-29 21:40:04 +0100

    Exercise 12: Mine

--- JavaScripting/app.js
+++ JavaScripting/app.js
@@ -1,3 +1,9 @@
-﻿var pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+﻿var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(pizzaToppings);
+function evenNumbers(number) {
+	return number % 2 === 0;
+}
+
+var filtered = numbers.filter(evenNumbers);
+
+console.log(filtered);

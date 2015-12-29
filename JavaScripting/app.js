f5d673fd314f993a110033bd57f1060c38e9e28c
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-29 21:36:13 +0100

    Exercise 11: Mine

--- JavaScripting/app.js
+++ JavaScripting/app.js
@@ -1,8 +1,3 @@
-﻿var total = 0;
-var limit = 10;
+﻿var pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
 
-for (var i = 0; i < limit; i++) {
-	total += i
-}
-
-console.log(total);
+console.log(pizzaToppings);

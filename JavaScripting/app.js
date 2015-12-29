75803f3fc3614bc8a375b3a26864894c6497e2e9
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-29 21:41:50 +0100

    Exercise 13: Mine

--- JavaScripting/app.js
+++ JavaScripting/app.js
@@ -1,9 +1,3 @@
-﻿var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+﻿var food = ["apple", "pizza", "pear"];
 
-function evenNumbers(number) {
-	return number % 2 === 0;
-}
-
-var filtered = numbers.filter(evenNumbers);
-
-console.log(filtered);
+console.log(food[1]);

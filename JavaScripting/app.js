9b66efaabfc56ba15913b15f91870bd05d77592a
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-06 20:23:01 +0100

    Exercise 19: Mine

--- JavaScripting/app.js
+++ JavaScripting/app.js
@@ -1,5 +1,16 @@
-﻿var math = function (x, y, z) {
-	return x + (y * z);
-}
+﻿var a = 1, b = 2, c = 3;
 
-console.log(math(53, 61, 67));
+(function firstFunction() {
+	var b = 5, c = 6;
+	(function secondFunction() {
+		var b = 8;
+		console.log("a: " + a + ", b: " + b + ", c: " + c);
+		(function thirdFunction() {
+			var a = 7, c = 9;
+			(function fourthFunction() {
+				var a = 1, c = 8;
+			})();
+		})();
+	})();
+})();
+

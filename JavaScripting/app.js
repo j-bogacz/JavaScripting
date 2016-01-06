1fd95468defa7a492290d465c79437590d29a3c2
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-06 19:46:23 +0100

    Exercise 16: Mine

--- JavaScripting/app.js
+++ JavaScripting/app.js
@@ -1,7 +1,5 @@
-﻿var pizza = {
-	toppings: ["cheese", "sauce", "pepperoni"],
-	crust: "deep dish",
-	serves: 2
+﻿var food = {
+	types: "only pizza"
 }
 
-console.log(pizza);
+console.log(food["types"]);
